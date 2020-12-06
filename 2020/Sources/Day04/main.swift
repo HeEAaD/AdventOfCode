14f65129b1b2062e4742600fb62c5b485c16331e
Steffen Matthischke <steffen.matthischke@lovoo.com>
2020-12-06 09:54:43 +0100

    Refactor 2020 Day04

--- 2020/Sources/Day04/main.swift
+++ 2020/Sources/Day04/main.swift
@@ -9,58 +9,52 @@ let requiredFields = [
     //"cid", //  Country ID)
 ]
 
-var rawPassport: [String.SubSequence] = []
-var passports: [[String: String]] = []
-let lines = input.split(omittingEmptySubsequences: false, whereSeparator: \.isNewline)
-for (i, line) in lines.enumerated() {
-    if !line.isEmpty {
-        rawPassport.append(line)
-    }
+let groups = input
+    .split(omittingEmptySubsequences: false, whereSeparator: \.isNewline)
+    .split(separator: "")
 
-    if line.isEmpty || i == lines.count - 1 {
-        let fields = rawPassport
+let passports: [[String: String]] = groups
+    .map { group in
+        let fields = group
             .joined(separator: " ")
             .split(separator: " ")
             .map { field -> (String, String) in
                 let split = field.split(separator: ":")
                 return (String(split[0]), String(split[1]))
             }
-        let passport = Dictionary(fields, uniquingKeysWith: { (first, _) in first })
-        print(passport)
-        if requiredFields.allSatisfy({ passport.keys.contains($0) }) {
-            passports.append(passport)
-        }
-        rawPassport = []
+        return Dictionary(fields, uniquingKeysWith: { (first, _) in first })
     }
-}
+    .filter { passport in
+        requiredFields.allSatisfy({ passport.keys.contains($0) })
+    }
 
 print("Answer 1: \(passports.count)")
 
-let validPassports = passports.reduce(0) { validCount, passport in
-    guard let byr = Int(passport["byr"]!), byr >= 1920, byr <= 2002 else { return validCount }
-    guard let iyr = Int(passport["iyr"]!), iyr >= 2010, iyr <= 2020 else { return validCount }
-    guard let eyr = Int(passport["eyr"]!), eyr >= 2020, eyr <= 2030 else { return validCount }
+let validSum = passports.reduce(into: 0) { validSum, passport in
+    guard let byr = Int(passport["byr"]!), byr >= 1920, byr <= 2002 else { return }
+    guard let iyr = Int(passport["iyr"]!), iyr >= 2010, iyr <= 2020 else { return }
+    guard let eyr = Int(passport["eyr"]!), eyr >= 2020, eyr <= 2030 else { return }
 
     let hgt = passport["hgt"]!
     if hgt.hasSuffix("cm") {
-        guard let hgtInt = Int(hgt.dropLast(2)), hgtInt >= 150, hgtInt <= 193 else { return validCount }
+        guard let hgtInt = Int(hgt.dropLast(2)), hgtInt >= 150, hgtInt <= 193 else { return }
     } else if hgt.hasSuffix("in") {
-        guard let hgtInt = Int(hgt.dropLast(2)), hgtInt >= 59, hgtInt <= 76 else { return validCount }
+        guard let hgtInt = Int(hgt.dropLast(2)), hgtInt >= 59, hgtInt <= 76 else { return }
     } else {
-        return validCount
+        return
     }
 
     let hcl = passport["hcl"]!
     let hclColor = hcl.dropFirst()
-    guard hcl.hasPrefix("#"), hclColor.count == 6, hclColor.allSatisfy({ "0123456789abcdef".contains($0) }) else { return validCount }
+    guard hcl.hasPrefix("#"), hclColor.count == 6, hclColor.allSatisfy({ "0123456789abcdef".contains($0) }) else { return }
 
     let ecl = passport["ecl"]!
-    guard ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].contains(ecl) else { return validCount }
+    guard ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].contains(ecl) else { return }
 
     let pid = passport["pid"]!
-    guard pid.count == 9, pid.allSatisfy({ "0123456789".contains($0) }) else { return validCount }
+    guard pid.count == 9, pid.allSatisfy({ "0123456789".contains($0) }) else { return }
 
-    return validCount + 1
+    validSum += 1
 }
 
-print("Answer 2: \(validPassports)")
+print("Answer 2: \(validSum)")
