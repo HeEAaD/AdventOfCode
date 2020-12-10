db273e7679b59db7a183472d761bfb3ce81a2293
Steffen Matthischke <steffen.matthischke@lovoo.com>
2020-12-10 22:53:50 +0100

    Refactor 2020 Day10

--- 2020/Sources/Day10/main.swift
+++ 2020/Sources/Day10/main.swift
@@ -2,32 +2,18 @@ var adapters = input.split(whereSeparator: \.isNewline).map { Int($0)! }.sorted(
 
 adapters.append(adapters.last! + 3)
 
-var joltage = 0
 var diff1Sum = 0
 var diff3Sum = 0
-
-for adapter in adapters {
-    switch adapter - joltage {
-    case 1:
-        diff1Sum += 1
-    case 3:
-        diff3Sum += 1
-    default:
-        fatalError("invalid diff")
-    }
-    joltage = adapter
-}
-
-print("Answer 1: \(diff1Sum * diff3Sum)")
-
-joltage = 0
+var joltage = 0
 var diff1RowCount = 1
 var possibilities = 1
 for adapter in adapters {
     switch adapter - joltage {
     case 1:
+        diff1Sum += 1
         diff1RowCount += 1
     case 3:
+        diff3Sum += 1
         switch diff1RowCount {
         case 1, 2:
             break
@@ -47,4 +33,5 @@ for adapter in adapters {
     joltage = adapter
 }
 
+print("Answer 1: \(diff1Sum * diff3Sum)")
 print("Answer 2: \(possibilities)")
